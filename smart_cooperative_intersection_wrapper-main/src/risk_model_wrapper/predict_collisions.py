c92a959934776e79118bda040702ac3d9a210957
dikshant-honda <dikshant_s@jp.honda>
2023-03-02 16:13:28 +0900

    temporarily working code

--- smart_cooperative_intersection_wrapper-main/src/risk_model_wrapper/predict_collisions.py
+++ smart_cooperative_intersection_wrapper-main/src/risk_model_wrapper/predict_collisions.py
@@ -21,6 +21,8 @@ from .model import DataPoint
 from risk_model.plot.risk import plot_risk
 from risk_model.plot.animate import animate
 
+from .lane_info import *
+
 
 def _find_config(config_type: str) -> UncertaintyConfig:
     object_type = {
@@ -43,50 +45,52 @@ def predict_collisions(
     uncertain_ego_trajectory = Trajectory.static_ego(delta_t=delta_t, trajectory_length=trajectory_length).uncertain(
         UNCERTAINTY_CONFIG[ObjectType.PEDESTRIAN]
     )
-
-    uncertain_trajectories = [
-        Trajectory.future_prediction(
-            trajectory_id=datapoint.id,
-            position=datapoint.position,
-            velocity=datapoint.velocity,
-            delta_t=delta_t,
-            trajectory_length=trajectory_length,
-        ).uncertain(_find_config(datapoint.type) if with_types else _find_config("car"))
-        for datapoint in datapoints
-    ]
-    
-    events = calculate_overlaps(uncertain_ego_trajectory, uncertain_trajectories)
-
-    # result = []
-    # # compare risk with respect to every other vehicle
-    # for ego in datapoints:
-    #     uncertain_ego_trajectory = Trajectory.future_prediction(
-    #         trajectory_id=ego.id,
-    #         position=ego.position,
-    #         velocity=ego.velocity,
-    #         delta_t=delta_t,
-    #         trajectory_length=trajectory_length,
-    #     ).uncertain(_find_config(ego.type) if with_types else _find_config("car"))
-
-    #     uncertain_trajectories = []
+    waypoints = [waypoints_2_left, waypoints_2_straight, waypoints_2_right]
+    for waypoint in waypoints:
+        uncertain_trajectories = [
+            Trajectory.future_prediction(
+                trajectory_id=datapoint.id,
+                position=datapoint.position,
+                velocity=datapoint.velocity,
+                delta_t=delta_t,
+                trajectory_length=trajectory_length,
+                waypoints=waypoint,
+            ).uncertain(_find_config(datapoint.type) if with_types else _find_config("car"))
+            for datapoint in datapoints
+        ]
         
-    #     for traffic in datapoints:
-    #         if traffic != ego: 
-    #             uncertain_trajectories.append(
-    #             Trajectory.future_prediction(
-    #                 trajectory_id=traffic.id,
-    #                 position=traffic.position,
-    #                 velocity=traffic.velocity,
-    #                 delta_t=delta_t,
-    #                 trajectory_length=trajectory_length,
-    #             ).uncertain(_find_config(traffic.type) if with_types else _find_config("car")))
+        events = calculate_overlaps(uncertain_ego_trajectory, uncertain_trajectories)
 
-    #     events = calculate_overlaps(uncertain_ego_trajectory, uncertain_trajectories)
-    #     probability = compute_survival(events, delta_t=delta_t)
-    #     result.append((ego, probability))
+        # result = []
+        # # compare risk with respect to every other vehicle
+        # for ego in datapoints:
+        #     uncertain_ego_trajectory = Trajectory.future_prediction(
+        #         trajectory_id=ego.id,
+        #         position=ego.position,
+        #         velocity=ego.velocity,
+        #         delta_t=delta_t,
+        #         trajectory_length=trajectory_length,
+        #     ).uncertain(_find_config(ego.type) if with_types else _find_config("car"))
 
-        # getting the risks as a list, is it the risk with respect to other vehicle. how can i define my own risk value.
-    animate(uncertain_ego_trajectory, uncertain_trajectories)
-    # plot_risk(list(zip(datapoints, compute_survival(events, delta_t=delta_t))))
+        #     uncertain_trajectories = []
+            
+        #     for traffic in datapoints:
+        #         if traffic != ego: 
+        #             uncertain_trajectories.append(
+        #             Trajectory.future_prediction(
+        #                 trajectory_id=traffic.id,
+        #                 position=traffic.position,
+        #                 velocity=traffic.velocity,
+        #                 delta_t=delta_t,
+        #                 trajectory_length=trajectory_length,
+        #             ).uncertain(_find_config(traffic.type) if with_types else _find_config("car")))
+
+        #     events = calculate_overlaps(uncertain_ego_trajectory, uncertain_trajectories)
+        #     probability = compute_survival(events, delta_t=delta_t)
+        #     result.append((ego, probability))
+
+            # getting the risks as a list, is it the risk with respect to other vehicle. how can i define my own risk value.
+        animate(uncertain_ego_trajectory, uncertain_trajectories)
+        # plot_risk(list(zip(datapoints, compute_survival(events, delta_t=delta_t))))
     return list(zip(datapoints, compute_survival(events, delta_t=delta_t)))
     # return result
